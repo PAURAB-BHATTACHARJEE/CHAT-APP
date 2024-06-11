61696c428bfac421890240635cdb917a0f5c394b
Paurab Bhattacharjee <104007722+PAURAB-BHATTACHARJEE@users.noreply.github.com>
2024-06-12 01:19:29 +0530

    Add files via upload

---
+++ src/main/java/com/Spring_Projects/Spring_Boot_chat_app/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Projects.Spring_Boot_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppApplication.class, args);
+	}
+
+}
